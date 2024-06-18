71e93f877b544caaa69bd698ca127cc8c49f60ac
Douglas Melo <douglasmnegri@gmail.com>
2024-06-18 19:47:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(key = null, left = null, right = null) {
+    this.key = key;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array) {
+        
+  }
+}
